4583a061b538709b497375a132245e45a49a4b3d
Divyadharshana5 <divyadharshana3@gmail.com>
2024-11-26 19:47:07 +0530

    initialize the Herosection

---
+++ src/app/Components/HeroSection.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const HeroSection = () => {
+  return (
+    <section>
+      <h1 className="text-white mb-4 text-6xl font-extrabold">
+        Hello,We are Developers
+      </h1>
+      <p className="text-[#ADB7BE]">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem vero
+        nisi praesentium, fuga repellat temporibus neque consequuntur nobis
+        vitae tenetur?
+      </p>
+    </section>
+  );
+};
+
+export default HeroSection;
