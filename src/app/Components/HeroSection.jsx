f0414017e56bfa8aca3e1db53e9f1013f16e046f
Divyadharshana5 <divyadharshana3@gmail.com>
2024-11-28 09:17:00 +0530

    update the page.js

--- src/app/Components/HeroSection.jsx
+++ src/app/Components/HeroSection.jsx
@@ -4,9 +4,9 @@ import Image from "next/image";
 const HeroSection = () => {
   return (
     <section>
-      <div className="grid grid-cols-1 lg:grid-cols-12">
-        <div className="col-span-7 place-self-center">
-          <h1 className="text-white text-4xl mb-4 lg:text-6xl font-extrabold">
+      <div className="grid grid-cols-1 sm:grid-cols-12">
+        <div className="col-span-7 place-self-center text-center sm:text-left">
+          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
             Hello,We are Developers
           </h1>
           <p className="text-[#ADB7BE] text-lg mb-6 lg:text-xl">
